c12be88e457ab4529cde2f9cf5f1841b35de5885
Julio Suarez <julio.suarez91@gmail.com>
2024-01-23 02:12:10 -0400

    ssl

--- resources/views/VistaEmail/index.blade.php
+++ resources/views/VistaEmail/index.blade.php
@@ -24,7 +24,7 @@
 
         <ol class="list-decimal list-inside text-lg mb-6">
             <li>Haz clic en el siguiente enlace para registrarte en nuestra plataforma:
-                <a href="https://jorgeballivian.cloud/">DIAGRAMADOR UML</a>
+                <a href="http://jorgeballivian.cloud/">DIAGRAMADOR UML</a>
             </li>
             <li>Completa el proceso de registro y crea tu perfil.</li>
             <li>Ve a la seccion de diagramas y podras ver la lista de diagramas en colaboracion.</li>
