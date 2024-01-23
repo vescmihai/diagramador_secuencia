598b72d394cd4affdb991d49acdd24bc7a4e6029
Julio Suarez <julio.suarez91@gmail.com>
2024-01-23 11:00:43 -0400

    commonjs

--- socket.js
+++ socket.js
@@ -1,6 +1,11 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import http from 'http';
+// import express from 'express';
+// import { Server } from 'socket.io';
+// import http from 'http';
+
+const express = require('express');
+const { Server } = require('socket.io');
+const http = require('http');
+
 
 const app = express();
 
